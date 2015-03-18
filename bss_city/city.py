297c053c80359997187bb537154ea8b2c502e9b2
Patrick Tombez <patrick.tombez@blues2.ch>
2015-03-18 17:02:09 +0100

    Put swiss cities in first in search forms

--- bss_city/city.py
+++ bss_city/city.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2012-2013 Bluestar Solutions Sàrl (<http://www.blues2.ch>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -28,14 +28,22 @@ ZIP_TYPES = [('10', 'Home addresses and PO Box'),
              ('80', 'Internal for the Post')]
 
 class bluestar_city(osv.osv):
-   
+
     _name = "bluestar.city"
     _description = "City"
 
     def _get_name(self, cr, uid, ids, field_name, arg, context):
-        result={}
+        result = {}
         for city in self.browse(cr, uid, ids, context=context):
-            result[city.id] = '%(zip)s %(city)s' % {'zip': city.zip ,'city': city.long_name}
+            result[city.id] = '%(zip)s %(city)s' % {'zip': city.zip , 'city': city.long_name}
+        return result
+
+    def _get_is_swiss(self, cr, uid, ids, field_name, arg, context):
+        result = {}
+        res_country_obj = self.pool.get('res.country')
+        swiss_country_id = res_country_obj.search(cr, uid, [('code', '=', 'CH')], context=context)
+        for city in self.browse(cr, uid, ids, context=context):
+            result[city.id] = city.country_id.id in swiss_country_id
         return result
 
     _columns = {
@@ -47,10 +55,11 @@ class bluestar_city(osv.osv):
         'state_id': fields.many2one('res.country.state', 'State'),
         'country_id': fields.many2one('res.country', 'Country'),
         'name': fields.function(_get_name, type='char', method=True, store=True, string='Name'),
+        'is_swiss': fields.function(_get_is_swiss, type='boolean', method=True, store=True)
     }
-    
-    _order = 'country_id, name asc'
-                    
+
+    _order = 'is_swiss DESC, country_id, name asc'
+
 bluestar_city()
 
 class bluestar_partner(osv.osv):
@@ -59,24 +68,24 @@ class bluestar_partner(osv.osv):
     _description = "Bluestar City Partner"
 
     _columns = {
-        'city_id': fields.many2one('bluestar.city', 
+        'city_id': fields.many2one('bluestar.city',
                                    'City Search',
-                                   domain="[('zip_type', 'not in', ['80'])]", 
+                                   domain="[('zip_type', 'not in', ['80'])]",
                                    required=False, store=False),
     }
 
-    def onchange_city_id(self, cr, uid, ids, city_id): 
-        v={}
-        
+    def onchange_city_id(self, cr, uid, ids, city_id):
+        v = {}
+
         if city_id:
             city = self.pool.get('bluestar.city').browse(cr, uid, city_id)
             v['zip'] = city.zip
             v['city'] = city.long_name
-            v['country_id'] = city.country_id.id 
+            v['country_id'] = city.country_id.id
             if city.state_id:
-                v['state_id'] = city.state_id.id 
+                v['state_id'] = city.state_id.id
             v['city_id'] = None
-        
-        return {'value': v} 
-    
+
+        return {'value': v}
+
 bluestar_partner()
