3ad68d0ecd5749360a8db382190a48f84dc29eba
Daniel Le Gall <daniel.legall@blues2.ch>
2015-04-09 16:02:28 +0200

    correct version

--- bss_city_ch/__openerp__.py
+++ bss_city_ch/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Swiss Cities',
-    'version': '7.0.1.0',
+    'version': '7.0.1.0-20150409',
     "category" : 'Bluestar/Generic module',
     'complexity': "easy",
     'description': """
