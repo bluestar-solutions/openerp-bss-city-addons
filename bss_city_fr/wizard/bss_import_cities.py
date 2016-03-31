70b820ceee56dfad60e029f7a98c9a2476d58c83
Hervé Martinet <herve.martinet@blues2.ch>
2016-03-31 15:27:36 +0200

    OELPBUG-309 Fix bad charset for CH cities file

--- bss_city_fr/wizard/bss_import_cities.py
+++ bss_city_fr/wizard/bss_import_cities.py
@@ -38,7 +38,8 @@ class bss_import_cities(osv.TransientModel):
             context = {}
 
         file_input = io.BytesIO(base64.b64decode(wiz.file))
-        reader = self.win1250_csv_reader(file_input, delimiter=';')
+        reader = self.decoded_csv_reader(file_input, 'windows-1250',
+                                         delimiter=';')
 
         headers = reader.next()
         if len(headers) != 4:
